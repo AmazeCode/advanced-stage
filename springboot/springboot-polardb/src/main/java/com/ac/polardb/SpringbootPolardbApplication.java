e44e4dfcfd8cec2719a288476ce4d19d29e95ed2
zhangyadong <1558628010@qq.com>
2024-05-05 20:49:45 +0800

    feat:springboot连接polradb-for-postgresql

---
+++ springboot/springboot-polardb/src/main/java/com/ac/polardb/SpringbootPolardbApplication.java
@@ -0,0 +1,13 @@
+package com.ac.polardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPolardbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPolardbApplication.class, args);
+    }
+
+}
---
+++ springboot/springboot-polardb/src/test/java/com/ac/polardb/SpringbootPolardbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.polardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPolardbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
